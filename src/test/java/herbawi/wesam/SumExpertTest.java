30e0ad13ac86b346a277051b5ecfd104c825071c
Wesam Said Herbawi <wesam.saidherbawi@here.com>
2023-03-04 21:36:56 +0100

    Fix tests

--- src/test/java/herbawi/wesam/SumExpertTest.java
+++ src/test/java/herbawi/wesam/SumExpertTest.java
@@ -20,7 +20,7 @@ class SumExpertTest {
         SumExpert sumExpert = new SumExpert();
         int sum = sumExpert.sum(5, -5, -5, 5);
 
-        assertEquals(10, sum);
+        assertEquals(0, sum);
 
     }
 
