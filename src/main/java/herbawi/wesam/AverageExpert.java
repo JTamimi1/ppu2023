300af754164f62794dab4897530935ce16b422a1
ma3ali <ma3ali@ppu.edu>
2023-03-06 19:34:27 +0200

    Ma3ali Commit 1

--- src/main/java/herbawi/wesam/AverageExpert.java
+++ src/main/java/herbawi/wesam/AverageExpert.java
@@ -1,11 +1,12 @@
 package herbawi.wesam;
 
 public class AverageExpert {
+    private SumExpert sumExpert;
     public AverageExpert(SumExpert sumExpert) {
         this.sumExpert = sumExpert;
     }
 
-    private SumExpert sumExpert;
+
 
     public double avg(int... a) {
         return sumExpert.sum(a) / a.length;
--- src/main/java/herbawi/wesam/Main.java
+++ src/main/java/herbawi/wesam/Main.java
@@ -2,6 +2,13 @@ package herbawi.wesam;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = {1, 2, 3, 4, 5};
+        SumExpert sumExpert = new SumExpert();
+        AverageExpert averageExpert = new AverageExpert(sumExpert);
+        DivisionExpert divisionExpert =new DivisionExpert();
+        System.out.println("sum = " + sumExpert.sum(numbers));
+        System.out.println("average= "+ averageExpert.avg(numbers));
+        System.out.println(numbers[3]+"/" + numbers[1] + "= " +  divisionExpert.divide(numbers[3],numbers[1]));
+
     }
 }
