5e966683bf11bd33ab85ed2c23dacc0e4b49693f
rafat86 <rnaserdeen@ppu.edu>
2023-03-08 09:52:00 +0200

    Update Main.java

--- src/main/java/herbawi/wesam/Main.java
+++ src/main/java/herbawi/wesam/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("sum = " + sumExpert.sum(numbers));
         System.out.println("average= "+ averageExpert.avg(numbers));
         System.out.println(numbers[3]+"/" + numbers[1] + "= " +  divisionExpert.divide(numbers[3],numbers[1]));
+        System.out.println("Heloo Bro !");
 
     }
 }
