cbb1a3e9ccc980dc6486bf75ddfa6a82b0973be9
LanaSharabati <lanasharabati99@gmail.com>
2023-03-10 22:54:22 +0200

    Added an error to test the build

--- src/main/java/herbawi/wesam/DivisionExpert.java
+++ src/main/java/herbawi/wesam/DivisionExpert.java
@@ -6,6 +6,6 @@ public class DivisionExpert {
     public double divide(int a, int b){
         if(b==0)
             throw new IllegalArgumentException("b cannot be 0");
-        return a / b;
+        return a * b;
     }
 }
