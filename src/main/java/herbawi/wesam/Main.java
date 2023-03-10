ef175d8876053ee620972730e73e1a2c418351c8
Shadi Atawneh <shadi@ppu.edu>
2023-03-10 21:06:30 +0200

    remove extra print

--- src/main/java/herbawi/wesam/Main.java
+++ src/main/java/herbawi/wesam/Main.java
@@ -9,7 +9,6 @@ public class Main {
         System.out.println("sum = " + sumExpert.sum(numbers));
         System.out.println("average= "+ averageExpert.avg(numbers));
         System.out.println(numbers[3]+"/" + numbers[1] + "= " +  divisionExpert.divide(numbers[3],numbers[1]));
-        System.out.println("Heloo Bro !");
-        System.out.println("conflice_rafat !");
+
     }
 }
