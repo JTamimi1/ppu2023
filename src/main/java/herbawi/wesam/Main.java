f1ecbf5c562ebae89b92c4a1572b907fa09c9c63
jihad <j.tamimi@ppu.edu>
2023-03-14 20:09:49 +0200

    Testing Endpoint

--- src/main/java/herbawi/wesam/Main.java
+++ src/main/java/herbawi/wesam/Main.java
@@ -1,9 +1,15 @@
 package herbawi.wesam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Main {
+    @GetMapping("/message")
+    public String message(){
+        return "Congrats ! your application deployed successfully in Azure Platform. !" ;
+    }
+
     public static void main(String[] args) {
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        SumExpert sumExpert = new SumExpert();
