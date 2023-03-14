6946aa25ffbb7e33926fd164bbc0636bf5443a4d
jihad <j.tamimi@ppu.edu>
2023-03-14 19:57:07 +0200

    adding target

--- src/main/java/herbawi/wesam/Main.java
+++ src/main/java/herbawi/wesam/Main.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        SumExpert sumExpert = new SumExpert();
-        AverageExpert averageExpert = new AverageExpert(sumExpert);
-        DivisionExpert divisionExpert =new DivisionExpert();
-        System.out.println("sum = " + sumExpert.sum(numbers));
-        System.out.println("average= "+ averageExpert.avg(numbers));
-        System.out.println(numbers[3]+"/" + numbers[1] + "= " +  divisionExpert.divide(numbers[3],numbers[1]));
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        SumExpert sumExpert = new SumExpert();
+//        AverageExpert averageExpert = new AverageExpert(sumExpert);
+//        DivisionExpert divisionExpert =new DivisionExpert();
+//        System.out.println("sum = " + sumExpert.sum(numbers));
+//        System.out.println("average= "+ averageExpert.avg(numbers));
+//        System.out.println(numbers[3]+"/" + numbers[1] + "= " +  divisionExpert.divide(numbers[3],numbers[1]));
         SpringApplication.run(Main.class, args);
     }
 }
