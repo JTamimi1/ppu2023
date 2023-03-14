e1645b3520356ccbeb7bcea19e8e68337d7f592c
jihad <j.tamimi@ppu.edu>
2023-03-14 20:19:18 +0200

    Adding RestController to main

--- src/main/java/herbawi/wesam/Main.java
+++ src/main/java/herbawi/wesam/Main.java
@@ -2,8 +2,10 @@ package herbawi.wesam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     @GetMapping("/message")
     public String message(){
